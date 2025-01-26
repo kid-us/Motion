d4c7622552afd4fa1caa17138d62ef639cf27e01
kid-us <kidushh29@gmail.com>
2025-01-27 02:02:45 +0300

    While drag

--- src/components/Gesture/WhileDrag.tsx
+++ src/components/Gesture/WhileDrag.tsx
@@ -4,7 +4,21 @@ import { useRef } from "react";
 const WhileDrag = () => {
   const constraintRef = useRef(null);
   return (
-    <>
+    <div className="grid grid-cols-3 gap-x-5">
+      <motion.div
+        ref={constraintRef}
+        className="h-[100px] w-14 border rounded-3xl overflow-hidden"
+      >
+        <motion.div
+          drag="y"
+          dragConstraints={constraintRef}
+          dragElastic={0}
+          dragMomentum={false}
+          animate={{ width: ["200px"], height: ["100px"] }}
+          className="bg-white cursor-pointer m-0 p-0"
+        ></motion.div>
+      </motion.div>
+
       <motion.div
         drag="x"
         whileDrag={{ scale: 1.2 }}
@@ -23,7 +37,7 @@ const WhileDrag = () => {
           className="w-[100px] h-[100px] border border-gray-400 rounded bg-white"
         ></motion.div>
       </motion.div>
-    </>
+    </div>
   );
 };
 
